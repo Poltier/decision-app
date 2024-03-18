131eb851d23ac752f9508dcc81c051fe3a0fd363
Pol Tosca <poltr96@gmail.com>
2024-03-18 08:37:52 +0100

    creación perfil

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
@@ -28,6 +30,8 @@ import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import { AngularFireModule } from '@angular/fire/compat';
 import { environment } from '../environments/environment';
 
+import { ProfileComponent } from './pages/profile/profile.component';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -38,6 +42,7 @@ import { environment } from '../environments/environment';
     AdminComponent,
     RegisterComponent,
     OptionCardComponent,
+    ProfileComponent,
     GameThematicComponent 
   ],
   imports: [
--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,10 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FirebaseService } from '../../services/firebase.service';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrl: './profile.component.css'
+  styleUrls: ['./profile.component.scss']
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+  profileForm: FormGroup;
+  avatars: string[] = [];
+  selectedAvatar: string = '';
+
+  constructor(
+    private fb: FormBuilder,
+    private firebaseService: FirebaseService
+  ) {
+    this.profileForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      newPassword: ['', [Validators.minLength(8)]],
+      displayName: [''],
+      avatar: ['']
+    });
+  }
+
+  ngOnInit(): void {
+    this.firebaseService.getAvatars().then((avatars) => {
+      this.avatars = avatars;
+      this.selectedAvatar = avatars[Math.floor(Math.random() * avatars.length)];
+    }).catch(error => {
+      console.error("Failed to get avatars:", error);
+    });
+  }
+
+  updateProfile(): void {
+    if (this.profileForm.valid) {
+      const { displayName, email, password, avatar } = this.profileForm.value;
+      // Update the user's profile data
+      this.firebaseService.updateProfileData(displayName, avatar).then(() => {
+        console.log('Profile data updated');
+      });
+
+      if (email) {
+        this.firebaseService.updateUserEmail(email).then(() => {
+          console.log('Email updated');
+        });
+      }
+
+      if (password) {
+        this.firebaseService.updateUserPassword(password).then(() => {
+          console.log('Password updated');
+        });
+      }
+    }
+  }
+
+  selectAvatar(avatarUrl: string): void {
+    this.selectedAvatar = avatarUrl;
+  }
 
 }
