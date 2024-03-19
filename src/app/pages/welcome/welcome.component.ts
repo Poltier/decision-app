1464a01410e93ff6a29eeafdbe1ff389d49009d6
Pol Tosca <poltr96@gmail.com>
2024-03-19 15:36:40 +0100

    mejoras visualización register, login, welcome

--- src/app/pages/welcome/welcome.component.ts
+++ src/app/pages/welcome/welcome.component.ts
@@ -21,6 +21,7 @@ export class WelcomeComponent {
     this.loginForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(8)]]
+      
     });
   }
 
